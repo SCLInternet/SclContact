02f9c77e490e0cdfcb1b44257fe3356e062ed6ac
tom <tom@x2k.co.uk>
2013-02-19 13:31:11 +0000

    Project setup

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 SclContact
 ==========
 
+[![Build Status](https://travis-ci.org/SCLInternet/SclContact.png?branch=master)](https://travis-ci.org/SCLInternet/SclZfContact)
+
 Set of classes for a contact details for a person.
---
+++ bin/travis_build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+### Run unit tests
+phpunit
+PHPUNIT=$?
+
+### Check coding standards
+vendor/bin/phpcs --standard=psr2 src
+PHPCS=$?
+
+### Check code quality
+vendor/bin/phpmd src text codesize
+PHPMD=$?
+
+### Check for license headers
+LICENSE=0
+
+LICENSE_HEADER="<?php
+/**
+ * SclContact (https://github.com/SCLInternet/SclContact)
+ *
+ * @link https://github.com/SCLInternet/SclContact for the canonical source repository
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */"
+
+for i in `find src tests -name '*.php'`; do
+    diff <(echo "$LICENSE_HEADER") <(head -7 "$i");
+
+    if [ "$?" -ne "0" ]; then
+        echo "Missing or invalid license header in \"$i\""
+        let LICENSE=1
+    fi
+done
+
+### Display results
+EXIT=0
+
+echo
+echo "#### RESULTS:"
+
+if [ "$PHPUNIT" -ne "0" ]; then
+    echo "**** Unit tests failed"
+    EXIT=1
+fi
+if [ "$PHPCS" -ne "0" ]; then
+    echo "**** Coding standards failed"
+    EXIT=1
+fi
+if [ "$PHPMD" -ne "0" ]; then
+    echo "**** Mess detection failed"
+    EXIT=1
+fi
+if [ "$LICENSE" -ne "0" ]; then
+    echo "**** License header check failed"
+    EXIT=1
+fi
+
+exit $EXIT
