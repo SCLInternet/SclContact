1a998ca8bac698708b7fb2cec4c8b67cb49a4ec6
Tom Oram <tom@scl.co.uk>
2013-05-28 11:32:54 +0100

    Add license header to Module.php

--- src/Module.php
+++ src/Module.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * SclContact (https://github.com/SCLInternet/SclContact)
+ *
+ * @link https://github.com/SCLInternet/SclContact for the canonical source repository
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
 namespace SclContact;
 
 use Zend\ModuleManager\Feature\FilterProviderInterface;
