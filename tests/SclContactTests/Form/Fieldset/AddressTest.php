e84f4f6f0d015b587eba12806cbb0aa28aae1044
Tom Oram <tom@scl.co.uk>
2013-06-02 20:40:59 +0100

    Added tests for form fieldsets

---
+++ tests/SclContactTests/Form/Fieldset/AddressTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * SclContact (https://github.com/SCLInternet/SclContact)
+ *
+ * @link https://github.com/SCLInternet/SclContact for the canonical source repository
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace SclContactTests\Form\Fieldset;
+
+use SclContact\Form\Fieldset\Address;
+
+/**
+ * Unit tests for the {@see Address} class.
+ *
+ * @author Tom Oram <tom@scl.co.uk>
+ */
+class AddressTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test the construction of the Address fieldset.
+     *
+     * @covers SclContact\Address::__construct
+     *
+     * @return void
+     */
+    public function testConstruction()
+    {
+        $fieldset = new Address();
+
+        $this->markTestIncomplete('Need to test elements are correctly added.');
+    }
+}
